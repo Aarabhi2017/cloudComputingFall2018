11513c8bf5ad860dfceb0eb3fa7ae6394cedb1a9
AVP <31553268+Aarabhi2017@users.noreply.github.com>
2018-11-14 22:54:08 -0800

    Committing changes

---
+++ studentInformationSystems4/src/main/java/com/csye/fall2018/studentInformationSystems/datamodels/DynamoDBConnector.java
@@ -0,0 +1,30 @@
+package com.csye.fall2018.studentInformationSystems.datamodels;
+
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+public class DynamoDBConnector {
+
+	static AmazonDynamoDB dynamoDB;
+
+	public static void init() {
+		if (dynamoDB == null) {
+			
+			
+			InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider(false);
+			credentialsProvider.getCredentials();
+
+			dynamoDB = AmazonDynamoDBClientBuilder.standard().withCredentials(credentialsProvider)
+					.withRegion("us-east-1").build();
+			
+			System.out.println("\nClient Created\n");
+		}
+	}
+
+	public AmazonDynamoDB getClient() {
+		return dynamoDB;
+	}
+
+}
